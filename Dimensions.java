82e51b9290e51809167e26a35a6cf69ee9f5da2e
ANDREOU ANDREAS <andreouandreas@andreas-mac.localdomain>
2019-05-25 15:59:06 +0200

    PC_Project

---
+++ Dimensions.java
@@ -0,0 +1,15 @@
+
+public class Dimensions {
+
+	private int width;
+	private int length;
+	private int depth;
+	public Dimensions(int width, int length, int depth) {
+		super();
+		this.width = width;
+		this.length = length;
+		this.depth = depth;
+	}
+	
+	
+}
